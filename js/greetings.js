c02516929b3b50c8ddd769f965d76108d7374429
Troy <yoon6@gmail.com>
2022-12-01 14:38:43 +0900

    greeting 작업

--- js/greetings.js
+++ js/greetings.js
@@ -10,7 +10,7 @@ function onLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings();
+    paintGreetings(username);
 }
 
 function paintGreetings(username) {
@@ -24,5 +24,5 @@ if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreetings()
+    paintGreetings(savedUsername);
 }
